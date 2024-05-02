3d85728a2f3086bded10d9a44ead3d7e0cd95671
Mamathashree K <92993827+Mamathashreek@users.noreply.github.com>
2024-05-02 14:15:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let ip = document.querySelector('input');
+
+function equal() {
+    let ipvalue = ip.value;
+    ip.value = eval(ipvalue);
+}
+
+function ac() {
+    ip.value = '';
+}
+
+function number(a) {
+    let ipvalue = ip.value;
+    ip.value =  ipvalue+a;
+}
